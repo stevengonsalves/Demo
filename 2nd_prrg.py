06c0364a00befaf7b72a5406f56d153b09f91d64
STEEVAN GONSALVES <107107422+stevengonsalves@users.noreply.github.com>
2025-08-03 13:05:53 +0530

    Create 2nd_prrg.py
    Different program

---
+++ 2nd_prrg.py
@@ -0,0 +1,9 @@
+num1_str = input("Enter the first number: ")
+num2_str = input("Enter the second number: ")
+
+# Convert input strings to numbers (integers or floats)
+num1 = float(num1_str)
+num2 = float(num2_str)
+
+sum_result = num1 + num2
+print("The sum of", num1, "and", num2, "is:", sum_result)
